e7d7e196849468f74ece615abf6974950b49bdea
AR WebDev <alessandrorigido5@gmail.com>
2026-04-01 12:59:48 +0200

    Aggiunto modulo login

--- gestionale/routes/web.php
+++ gestionale/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+// Rotte per ospiti (non autenticati)
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+});
+
+// Rotte protette (autenticate)
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 });

dba34a2960c06b1c01d43b75d2366d715b9ecfd4
AR WebDev <alessandrorigido5@gmail.com>
2026-04-07 10:56:28 +0200

    Aggiunta logica cestino e nuovo cliente/fornitore

--- gestionale/resources/views/livewire/admin/entities-table.blade.php
+++ gestionale/resources/views/livewire/admin/entities-table.blade.php
@@ -192,16 +192,19 @@
                     <tr class="hover:bg-gray-50 transition-colors duration-150">
                         <td class="px-6 py-4">
                             <div class="flex items-center">
-                                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-200 flex items-center justify-center">
-                                    <svg class="h-6 w-6 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                        @if($entity->entity_type == 'cliente')
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-                                        @elseif($entity->entity_type == 'fornitore')
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 18l4-4m0 0l4 4m-4-4v8m12 0h-8a4 4 0 00-4 4v8m8-4l4 4m0 0l4-4m-4 4V8a4 4 0 00-4-4H8a4 4 0 00-4 4v16m8-12h4"></path>
-                                        @else
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                                        @endif
-                                    </svg>
+                                <div class="flex-shrink-0 h-10 w-10 rounded-full flex items-center justify-center
+                                    @if($entity->entity_type == 'cliente') bg-green-100
+                                    @elseif($entity->entity_type == 'fornitore') bg-blue-100
+                                    @else bg-purple-100
+                                    @endif">
+                                    
+                                    @if($entity->entity_type == 'cliente')
+                                        <i class="fas fa-user text-green-600 text-lg"></i>
+                                    @elseif($entity->entity_type == 'fornitore')
+                                        <i class="fas fa-truck text-blue-600 text-lg"></i>
+                                    @else
+                                        <i class="fas fa-handshake text-purple-600 text-lg"></i>
+                                    @endif
                                 </div>
                                 <div class="ml-4">
                                     <div class="text-sm font-medium text-gray-900">
@@ -209,6 +212,7 @@
                                     </div>
                                     @if($entity->persona_riferimento)
                                     <div class="text-xs text-gray-500">
+                                        <i class="fas fa-user-tag mr-1 text-gray-400"></i>
                                         {{ $entity->persona_riferimento }}
                                     </div>
                                     @endif

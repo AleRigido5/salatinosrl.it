de97bdbed3f2f037f24baeb60485fea6a9edd996
AR WebDev <alessandrorigido5@gmail.com>
2026-04-13 12:00:04 +0200

    modificata pagina 2

--- gestionale/resources/js/app.js
+++ gestionale/resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
+
+// Importa Alpine
+import Alpine from 'alpinejs';
+
+// Rendi Alpine disponibile globalmente
+window.Alpine = Alpine;
+
+// Avvia Livewire (che inizializzerà anche Alpine)
+Livewire.start();
+
+// Se hai componenti Alpine personali, inizializzali dopo
+document.addEventListener('alpine:init', () => {
+    // I tuoi componenti Alpine qui
+});

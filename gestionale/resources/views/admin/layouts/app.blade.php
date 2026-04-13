81448de1c445fe4e3b8344f8e52c1069102ffc3a
AR WebDev <alessandrorigido5@gmail.com>
2026-04-13 07:19:23 +0200

    Modificato logo gestionale

--- gestionale/resources/views/admin/layouts/app.blade.php
+++ gestionale/resources/views/admin/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Admin Panel - @yield('title')</title>
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
